c10a32de6d9c9f6c1cd6405fc6b57124b98562ff
valeriia <dobrovolskavaleriia@gmail.com>
2025-05-19 17:20:00 +0200

    .

--- Django_project/settings.py
+++ Django_project/settings.py
@@ -163,8 +163,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
-MEDIA_URL = 'assets/img/'
-MEDIA_ROOT = BASE_DIR / 'img '
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 LOGIN_URL = 'account/login/'
