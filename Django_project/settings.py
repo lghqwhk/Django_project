6a9395ce0dcb34b509cbdabf85d7b9fabe876cd7
valeriia <dobrovolskavaleriia@gmail.com>
2025-05-19 17:07:45 +0200

    .

--- Django_project/settings.py
+++ Django_project/settings.py
@@ -158,14 +158,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
 MEDIA_URL = 'assets/img/'
 MEDIA_ROOT = BASE_DIR / 'img '
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+
 
 LOGIN_URL = 'account/login/'
+LOGOUT_URL = 'account/logout/'
 
 
 # Default primary key field type
