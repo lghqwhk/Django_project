65f05fd69d2c85b98d580a7fa323d7619eaecd0e
valeriia <dobrovolskavaleriia@gmail.com>
2025-05-20 19:29:42 +0200

    .

--- Django_project/settings.py
+++ Django_project/settings.py
@@ -28,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = False
+
 MODE = os.environ.get('MODE')
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '*')]
